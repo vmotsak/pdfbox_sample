d4bff11caf88767ead96c10aed5cea89cd7cdfef
Vladimir Motsak <vmotsak@gmail.com>
2014-01-15 17:14:23 +0200

    initial

---
+++ src/main/java/TextExtractor.java
@@ -0,0 +1,9 @@
+import org.apache.pdfbox.util.PDFTextStripper;
+
+import java.io.IOException;
+
+public class TextExtractor {
+    public static void main(String[] args) throws IOException {
+        PDFTextStripper stripper = new PDFTextStripper();
+    }
+}
